03aa44400f3321372a77b48c6e3be47421b993c7
David Estes <davydotcom@gmail.com>
2014-03-28 13:24:20 -0400

    updating to 0.2.0

--- grails-app/conf/SpudPermalinksUrlMappings.groovy
+++ grails-app/conf/SpudPermalinksUrlMappings.groovy
@@ -1,6 +1,6 @@
 class SpudPermalinksUrlMappings {
 
 	static mappings = {
-    "/spud/admin/permalinks"(resources: 'permalinks', namespace: 'spud_admin')
+	    "/spud/admin/permalinks"(resources: 'permalinks', namespace: 'spud_admin')
 	}
 }
